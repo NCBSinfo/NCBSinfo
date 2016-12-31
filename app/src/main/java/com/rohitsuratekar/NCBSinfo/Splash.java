598d4091e1b62c92a115b5fd287c277805c36e2f
Rohit Suratekar <rohitsuratekar@gmail.com>
2016-12-31 13:34:33 +0530

    Removing all old files for redisign

---
+++ app/src/main/java/com/rohitsuratekar/NCBSinfo/Splash.java
@@ -0,0 +1,13 @@
+package com.rohitsuratekar.NCBSinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
--- app/src/main/java/com/rohitsuratekar/NCBSinfo/activities/settings/eggs/breakout/Breakout.java
+++
@@ -1,9 +0,0 @@
-package com.rohitsuratekar.NCBSinfo.activities.settings.eggs.breakout;
-
-/**
- * NCBSinfo © 2016, Secret Biology
- * https://github.com/NCBSinfo/NCBSinfo
- * Created by Rohit Suratekar on 29-08-16.
- */
-public class Breakout {
-}
