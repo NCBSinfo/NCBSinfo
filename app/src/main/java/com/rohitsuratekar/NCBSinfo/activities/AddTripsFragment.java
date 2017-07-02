71cd3de39898acb9c4f997b5db08e8865b023689
Secret Admin <lastsecretofbiology@gmail.com>
2017-07-02 14:52:43 -0700

    Confirmation of trips layout added.

--- app/src/main/java/com/rohitsuratekar/NCBSinfo/activities/AddTripsFragment.java
+++ app/src/main/java/com/rohitsuratekar/NCBSinfo/activities/AddTripsFragment.java
@@ -1,6 +1,7 @@
 package com.rohitsuratekar.NCBSinfo.activities;
 
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,10 +11,16 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TimePicker;
 
 import com.rohitsuratekar.NCBSinfo.R;
+import com.secretbiology.helpers.general.Log;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Rohit on 7/2/2017.
@@ -21,14 +28,15 @@ import butterknife.ButterKnife;
 
 public class AddTripsFragment extends Fragment {
 
-    private RecyclerView recyclerView;
+    @BindView(R.id.et_trip_recycler)
+    RecyclerView recyclerView;
     private AddTripsAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.edit_transport_trips, container, false);
-        recyclerView = ButterKnife.findById(rootView, R.id.et_trip_recycler);
+        ButterKnife.bind(this, rootView);
         adapter = new AddTripsAdapter();
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
@@ -36,7 +44,22 @@ public class AddTripsFragment extends Fragment {
         recyclerView.addItemDecoration(new SwipeItemDecorator());
         ItemTouchHelper mItemTouchHelper = new ItemTouchHelper(TouchHelper.get(getContext(), recyclerView));
         mItemTouchHelper.attachToRecyclerView(recyclerView);
-        // ((AddTripsAdapter) recyclerView.getAdapter()).setUndoOn(true);
+        adapter.setUndoOn(true);
         return rootView;
+    }
+
+    @OnClick(R.id.et_pick_time)
+    public void openPicker() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                Log.inform();
+            }
+        }, hour, minute, false);//Yes 24 hour time
+        mTimePicker.show();
     }
 }
