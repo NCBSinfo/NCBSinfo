9b71783eec976527f4b65a4a32b1068f27e8d7f1
Secret Admin <lastsecretofbiology@gmail.com>
2017-10-06 10:28:54 -0700

    Added database and updated Readme.md

---
+++ app/src/main/java/com/rohitsuratekar/NCBSinfo/database/DatabaseMigration.java
@@ -0,0 +1,32 @@
+package com.rohitsuratekar.NCBSinfo.database;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.database.Cursor;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Rohit Suratekar on 20-06-17 for NCBSinfo.
+ * All code is released under MIT License.
+ */
+
+class DatabaseMigration {
+
+    List<RouteData> migrateToLatest(SupportSQLiteDatabase db) {
+        //// TODO: 20-06-17
+        List<RouteData> dataList = new ArrayList<>();
+        Log.i(getClass().getName(), "Database migration started");
+        Cursor cursor = db.query("SELECT * FROM routes", null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    Log.i(getClass().getName(), cursor.getString(cursor.getColumnIndex("origin")));
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
+        return dataList;
+    }
+}
