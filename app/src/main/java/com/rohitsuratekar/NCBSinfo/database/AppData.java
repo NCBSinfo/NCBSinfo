ddb2ce96ea1f7efa65fa07a25f227496d9014601
Rohit Suratekar <rohitsuratekar@gmail.com>
2017-10-24 14:21:45 +0530

    Added database migration

--- app/src/main/java/com/rohitsuratekar/NCBSinfo/database/AppData.java
+++ app/src/main/java/com/rohitsuratekar/NCBSinfo/database/AppData.java
@@ -6,13 +6,19 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
+import android.util.Log;
+import android.util.SparseArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Rohit Suratekar on 20-06-17 for NCBSinfo.
  * All code is released under MIT License.
  */
 
-@Database(entities = {RouteData.class, TripData.class}, version = 1)
+@Database(entities = {RouteData.class, TripData.class}, version = 10)
+//Changed from 9 to 10 in version 49
 public abstract class AppData extends RoomDatabase {
 
     private static final String DATABASE_NAME = "NCBSinfo";
@@ -27,6 +33,7 @@ public abstract class AppData extends RoomDatabase {
             INSTANCE =
                     Room.databaseBuilder(context.getApplicationContext(), AppData.class, DATABASE_NAME)
                             .addMigrations(MIGRATION_LATEST)
+                            .fallbackToDestructiveMigration() //Need this to remove all old database
                             .build();
         }
         return INSTANCE;
@@ -44,10 +51,40 @@ public abstract class AppData extends RoomDatabase {
         INSTANCE = null;
     }
 
-    private static final Migration MIGRATION_LATEST = new Migration(1,2) {
+    private static final Migration MIGRATION_LATEST = new Migration(9, 10) {
         @Override
         public void migrate(SupportSQLiteDatabase db) {
             new DatabaseMigration().migrateToLatest(db);
+            Log.i(getClass().getSimpleName(), "Old data collected. Creating new data");
+            List<DatabaseMigration.OldDataHolder> oldData = new DatabaseMigration().migrateToLatest(db);
+            SparseArray<List<DatabaseMigration.OldDataHolder>> dataCollection = new SparseArray<>();
+            //Collected all day rows
+            for (DatabaseMigration.OldDataHolder holder : oldData) {
+                List<DatabaseMigration.OldDataHolder> oldList = dataCollection.get(holder.getOldRouteNo(), new ArrayList<DatabaseMigration.OldDataHolder>());
+                oldList.add(holder);
+            }
+            //Convert to Route Data and Trip Data
+            for (int i = 0; i < dataCollection.size(); i++) {
+                RouteData data = new RouteData();
+                DatabaseMigration.OldDataHolder old = dataCollection.get(dataCollection.keyAt(i)).get(0);
+                data.setOrigin(old.getOrigin());
+                data.setDestination(old.getDestination());
+                data.setType(old.getType());
+                data.setCreatedOn(old.getCreatedOn());
+                data.setModifiedOn(old.getModifiedOn());
+                data.setSynced(old.getSynced());
+                data.setAuthor(old.getAuthor());
+                data.setFavorite(false);
+                long routeID = INSTANCE.routes().insertRoute(data);
+                for (DatabaseMigration.OldDataHolder oldDataHolder : dataCollection.get(dataCollection.keyAt(i))) {
+                    TripData trip = new TripData();
+                    trip.setDay(oldDataHolder.getDay());
+                    trip.setTrips(oldDataHolder.getTripData());
+                    trip.setRouteID((int) routeID);
+                    INSTANCE.trips().insertTrips(trip);
+                }
+            }
+            Log.i(getClass().getSimpleName(), "Database migrated successfully.");
         }
     };
 }
--- app/src/main/java/com/rohitsuratekar/NCBSinfo/database/DatabaseMigration.java
+++ app/src/main/java/com/rohitsuratekar/NCBSinfo/database/DatabaseMigration.java
@@ -3,6 +3,10 @@ package com.rohitsuratekar.NCBSinfo.database;
 import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.database.Cursor;
 import android.util.Log;
+import android.util.SparseArray;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,19 +18,78 @@ import java.util.List;
 
 class DatabaseMigration {
 
-    List<RouteData> migrateToLatest(SupportSQLiteDatabase db) {
-        //// TODO: 20-06-17
-        List<RouteData> dataList = new ArrayList<>();
-        Log.i(getClass().getName(), "Database migration started");
-        Cursor cursor = db.query("SELECT * FROM routes", null);
+    List<OldDataHolder> migrateToLatest(SupportSQLiteDatabase db) {
+        List<OldDataHolder> oldData = new ArrayList<>();
+        removeOld(db);
+        Log.i(getClass().getName(), "TransportTable Database migration started");
+        Cursor cursor = db.query("SELECT * FROM TransportTable", null);
         if (cursor != null) {
             if (cursor.moveToFirst()) {
                 do {
-                    Log.i(getClass().getName(), cursor.getString(cursor.getColumnIndex("origin")));
+                    OldDataHolder data = new OldDataHolder();
+                    data.setOrigin(cursor.getString(cursor.getColumnIndex("origin")));
+                    data.setDestination(cursor.getString(cursor.getColumnIndex("destination")));
+                    data.setType(cursor.getString(cursor.getColumnIndex("type")));
+                    data.setCreatedOn(cursor.getString(cursor.getColumnIndex("creation")));
+                    data.setModifiedOn(cursor.getString(cursor.getColumnIndex("modified")));
+                    data.setSynced(cursor.getString(cursor.getColumnIndex("synced")));
+                    data.setAuthor(cursor.getString(cursor.getColumnIndex("author")));
+                    List<String> trips = new Gson().fromJson(cursor.getString(cursor.getColumnIndex("trips")),
+                            new TypeToken<List<String>>() {
+                            }.getType());
+                    data.setTripData(trips);
+                    data.setOldRouteNo(cursor.getInt(cursor.getColumnIndex("route")));
+                    data.setDay(cursor.getInt(cursor.getColumnIndex("day")));
+                    data.setFavorite(false);
+                    oldData.add(data);
                 } while (cursor.moveToNext());
             }
             cursor.close();
         }
-        return dataList;
+        db.execSQL("DROP TABLE IF EXISTS TransportTable");
+        return oldData;
     }
+
+    private void removeOld(SupportSQLiteDatabase db) {
+        //Remove support from previous databases
+        Log.i(getClass().getName(), "Removing old databases");
+        db.execSQL("DROP TABLE IF EXISTS alarmTable");
+        db.execSQL("DROP TABLE IF EXISTS notifications");
+        db.execSQL("DROP TABLE IF EXISTS table_contacts");
+        db.execSQL("DROP TABLE IF EXISTS table_research_talk");
+        db.execSQL("DROP TABLE IF EXISTS table_notifications");
+    }
+
+    class OldDataHolder extends RouteData {
+
+        private List<String> tripData;
+        private int day;
+        private int oldRouteNo;
+
+        List<String> getTripData() {
+            return tripData;
+        }
+
+        void setTripData(List<String> tripData) {
+            this.tripData = tripData;
+        }
+
+        int getDay() {
+            return day;
+        }
+
+        void setDay(int day) {
+            this.day = day;
+        }
+
+        int getOldRouteNo() {
+            return oldRouteNo;
+        }
+
+        void setOldRouteNo(int oldRouteNo) {
+            this.oldRouteNo = oldRouteNo;
+        }
+    }
+
+
 }
